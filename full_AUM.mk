602a59a3bbf1432fa887a57f5be3ff81c8578d65
Khaled Necir <khaledannab@gmail.com>
2019-04-14 22:57:25 +0100

    Update full_AUM.mk

--- full_AUM.mk
+++ full_AUM.mk
@@ -11,11 +11,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.crypto.state=encrypted \
     ro.crypto.type=file
 
-$(shell mkdir -p $(OUT)/recovery/)
-$(shell mkdir -p $(OUT)/recovery/root/)
-$(shell touch $(OUT)/recovery/root/file_contexts)
+#$(shell mkdir -p $(OUT)/recovery/)
+#$(shell mkdir -p $(OUT)/recovery/root/)
+#$(shell touch $(OUT)/recovery/root/file_contexts)
 
- Hackery
+#Hackery
 $(shell mkdir -p $(OUT)/obj/ETC/sepolicy.recovery_intermediates)
 $(shell mkdir -p $(OUT)obj/ETC/sepolicy_intermediates) 
 $(shell cp device/honor/AUM/sepolicy $(OUT)/obj/ETC/sepolicy.recovery_intermediates/sepolicy.recovery)
